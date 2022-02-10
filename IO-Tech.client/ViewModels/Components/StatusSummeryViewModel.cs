b0e3a1257d4b28714551437bd63b54b474d84485
ayelletm-work <64577157+ayelletm-work@users.noreply.github.com>
2022-02-10 09:10:20 +0200

    Summery

---
+++ IO-Tech.client/ViewModels/Components/StatusSummeryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_Tech.client.ViewModels.Components
+{
+   public class StatusSummeryViewModel : ViewModelBase
+    {
+    }
+}
